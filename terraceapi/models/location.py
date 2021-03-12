fbf9f56f1654c12e7b5a849bfb0e43b170dd9e30
Jaron-Lane <jaronwohlwend@icloud.com>
2021-03-12 15:12:10 -0600

    Updated Models for no images. Yet

--- terraceapi/models/location.py
+++ terraceapi/models/location.py
@@ -5,5 +5,5 @@ class Location(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    photo = models.ImageField(_(""), upload_to=None, height_field=None, width_field=None, max_length=None)
+    # photo = models.ImageField(_(""), upload_to=None, height_field=None, width_field=None, max_length=None)
     lighting = models.CharField(max_length=20)
--- terraceapi/models/plant.py
+++ terraceapi/models/plant.py
@@ -8,6 +8,6 @@ class Plant(models.Model):
     nick_name = models.CharField(max_length=50)
     location = models.ForeignKey("Location", on_delete=models.CASCADE)
     bio = models.CharField(max_length=500)
-    photo = models.ImageField(_(""), upload_to=None, height_field=None, width_field=None, max_length=None)
+    # photo = models.ImageField(_(""), upload_to=None, height_field=None, width_field=None, max_length=None)
     watering_frequency = models.IntegerField()
     date_watered = models.DateField(_(""), auto_now=False, auto_now_add=False)
